44dfc84fd831924ce6431ca10b6ef0a6b45f4ba9
Christian Rieger <christian.rieger@student.tugraz.at>
2024-08-18 12:46:28 +0200

    [feature/handlers] make the handler implementation build

--- core/src/handler.rs
+++ core/src/handler.rs
@@ -23,6 +23,90 @@ macro_rules! all_the_tuples {
         $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15], T16);
     };
 }
+
+pub struct Body {
+    // kind: Kind,
+    // extra: Option<Box<Extra>>,
+}
+
+pub trait Handler<T, S, B = Body>: Clone + Send + Sized + 'static {
+    type Future: Future<Output = Response> + Send + 'static;
+
+    fn call(self, req: Request<B>, state: S) -> Self::Future;
+}
+
+#[async_trait::async_trait]
+impl<S, B, T> FromRequest<S, B, private::ViaParts> for T
+where
+    B: Send + 'static,
+    S: Send + Sync,
+    T: FromRequestParts<S>,
+{
+    type Rejection = <Self as FromRequestParts<S>>::Rejection;
+
+    async fn from_request(req: Request<B>, state: &S) -> Result<Self, Self::Rejection> {
+        let (mut parts, _) = req.into_parts();
+        Self::from_request_parts(&mut parts, state).await
+    }
+}
+
+pub struct Parts {}
+
+#[async_trait::async_trait]
+pub trait FromRequestParts<S>: Sized {
+    /// If the extractor fails it'll use this "rejection" type. A rejection is
+    /// a kind of error that can be converted into a response.
+    type Rejection: IntoResponse;
+
+    /// Perform the extraction.
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection>;
+}
+
+mod private {
+    #[derive(Debug, Clone, Copy)]
+    pub enum ViaParts {}
+
+    #[derive(Debug, Clone, Copy)]
+    pub enum ViaRequest {}
+}
+
+#[async_trait::async_trait]
+pub trait FromRequest<S, B, M = private::ViaRequest>: Sized {
+    /// If the extractor fails it'll use this "rejection" type. A rejection is
+    /// a kind of error that can be converted into a response.
+    type Rejection: IntoResponse;
+
+    /// Perform the extraction.
+    async fn from_request(req: Request<B>, state: &S) -> Result<Self, Self::Rejection>;
+}
+
+pub struct Response {}
+
+pub trait IntoResponse {
+    /// Create a response.
+    fn into_response(self) -> Response;
+}
+
+pub struct Request<T> {
+    head: Parts,
+    body: T,
+}
+
+impl<T> Request<T> {
+    #[inline]
+    pub fn into_parts(self) -> (Parts, T) {
+        (self.head, self.body)
+    }
+
+    #[inline]
+    pub fn from_parts(parts: Parts, body: T) -> Request<T> {
+        Request {
+            head: parts,
+            body: body,
+        }
+    }
+}
+
 impl<F, Fut, Res, S, B> Handler<((),), S, B> for F
 where
     F: FnOnce() -> Fut + Clone + Send + 'static,
